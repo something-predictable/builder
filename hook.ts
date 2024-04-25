5a2cd1625871642e1d1d327398117e4bbc2bfff6
k15r <git@riddance.k15r.xyz>
2024-04-26 01:14:17 +0200

    Don't throw on ping.

--- hook.ts
+++ hook.ts
@@ -1,16 +1,23 @@
-import { badRequest, objectSpreadable, post } from '@riddance/service/http'
+import { badRequest, notImplemented, objectSpreadable, post } from '@riddance/service/http'
 import { repoChanged } from './lib/events.js'
 
 post('webhook', async (context, request) => {
-    const { ref, before, after, repository } = objectSpreadable(request.body)
-    const { ssh_url: sshUrl } = objectSpreadable(repository)
-    if (
-        typeof ref !== 'string' ||
-        typeof before !== 'string' ||
-        typeof after !== 'string' ||
-        typeof sshUrl !== 'string'
-    ) {
-        throw badRequest()
+    const event = request.headers['x-github-event']
+    if (event === 'ping') {
+        //
+    } else if (event === 'push') {
+        const { ref, before, after, repository } = objectSpreadable(request.body)
+        const { ssh_url: sshUrl } = objectSpreadable(repository)
+        if (
+            typeof ref !== 'string' ||
+            typeof before !== 'string' ||
+            typeof after !== 'string' ||
+            typeof sshUrl !== 'string'
+        ) {
+            throw badRequest()
+        }
+        await repoChanged(context.env, sshUrl, ref, before, after)
+    } else {
+        throw notImplemented()
     }
-    await repoChanged(context.env, sshUrl, ref, before, after)
 })
